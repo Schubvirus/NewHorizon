f9fada49675ab22aed8560f1f14a35eff9ee6d70
Schubvirus <schubirus@gmail.com>
2023-04-03 22:43:36 +0200

    Auto f3x deletion

---
+++ src/server/f3x.server.ts
@@ -0,0 +1,16 @@
+import { Workspace, Players } from "@rbxts/services";
+
+const groupId = 7863870;
+const minRank = 6;
+
+Workspace.DescendantAdded.Connect((descendant) => {
+	if (descendant.IsA("Tool")) {
+		const player = Players.GetPlayerFromCharacter(descendant.Parent);
+		if (player) {
+			if (player.GetRankInGroup(groupId) < minRank) {
+				task.wait();
+				descendant.Destroy();
+			}
+		}
+	}
+});
