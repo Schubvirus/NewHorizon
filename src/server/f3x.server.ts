74e8e09cfeae4a99b1939102e8468db740447391
Schubvirus <schubirus@gmail.com>
2023-04-05 17:39:52 +0200

    forgot to add f3x name detection

--- src/server/f3x.server.ts
+++ src/server/f3x.server.ts
@@ -6,7 +6,7 @@ const minRank = 6;
 Workspace.DescendantAdded.Connect((descendant) => {
 	if (descendant.IsA("Tool")) {
 		const player = Players.GetPlayerFromCharacter(descendant.Parent);
-		if (player) {
+		if (player && descendant.Name.lower() === "building tools") {
 			if (player.GetRankInGroup(groupId) < minRank) {
 				task.wait();
 				descendant.Destroy();
